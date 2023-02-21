a8f5fe70fcad620a0f90f1c66af80d5b0fbaba76
Darya Belousova <belousmind@gmail.com>
2023-02-21 12:42:36 +0300

    feat: start solve the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,15 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        // create arr? 
     }
 
     guess() {
+        // return guess num
+        if (num === number) {
+            return num
+        }
 
     }
 
     lower() {
-
+        // 
     }
 
     greater() {
