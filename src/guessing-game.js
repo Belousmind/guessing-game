26991815b7f896ef7a541cc6e8a2f8d94a364f65
Darya Belousova <belousmind@gmail.com>
2023-09-02 23:22:46 +0200

    solved task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,27 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-        this.min = min;
-        this.max = max;
-        // create arr? 
+       this.min = min;
+       this.max = max; 
     }
-
     guess() {
-        // return guess num
-        if (num === number) {
-            return num
+        this.number;
+        this.midle = Math.round((this.max - this.min) / 2 + this.min)
+        if (this.number < this.midle) {
+            greater(); 
+        } else if (this.number > this.midle) {
+            lower();
+        } else {
+            return this.midle;
         }
-
     }
-
     lower() {
-        // 
+        this.max = this.midle;
     }
-
     greater() {
-
+        this.min = this.midle;
     }
 }
-
 module.exports = GuessingGame;
+
+
+
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let num = 4048;
+while(num > 0) {
+    num / 2
+    console.log(num)
+}
